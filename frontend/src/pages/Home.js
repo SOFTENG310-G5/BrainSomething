cb40e5dac985e6a19692cda834271a5a3f83884b
HamishPatel <hpat874@aucklanduni.ac.nz>
2024-08-17 22:05:00 +1200

    Added the basics for tile layout, currently for game 1

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // components
-import Game1 from "../components/Game1"
+import Game1 from "../components/Game1";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <div className="workouts">
-                {/* Render the minigame component */}
-                <Game1 />
-            </div>
+  return (
+    <div className="home">
+      <div className="cards-container">
+        {/* Card for Game 1 */}
+        <div className="card">
+          <h3>Game 1</h3>
+          <p>A fun jumping game</p>
+          <Link to="/Game1" className="play-link">
+            Play Game 1
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
