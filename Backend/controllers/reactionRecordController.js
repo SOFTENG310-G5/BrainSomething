6433d5ef27ba392c8da0d7e38511046e8a3eced9
RusiruD <rdha868@aucklanduni.ac.nz>
2024-09-19 20:50:41 +1200

    feat: Implemented fetching of scores saved in database for all games

--- Backend/controllers/reactionRecordController.js
+++ Backend/controllers/reactionRecordController.js
@@ -1,9 +1,28 @@
-const ReactionRecord = require('../models/reactionRecordModel');
+
+const ReactionGameRecords = require('../models/reactionRecordModel');
+const DinoJumpRecords = require('../models/dinoJumpRecordModel');
+const ChimpTestRecords = require('../models/chimpTestRecordModel');
+const ColourPuzzleRecords = require('../models/colourPuzzleRecordModel');
+
 
 // Get all reaction records
 const getAllRecords = async (req, res) => {
+    let records;
     try {
-        const records = await ReactionRecord.find({}).sort({ createdAt: -1 });
+        const screen = req.headers['screen'];
+        if(screen=="1"){
+            records = await DinoJumpRecords.find({}).sort({ createdAt: -1 });
+        }
+        else if(screen=="2"){
+            records = await ReactionGameRecords.find({}).sort({ createdAt: -1 });
+        }
+        else if(screen=="3"){
+            records = await ColourPuzzleRecords.find({}).sort({ createdAt: -1 });}
+        else if(screen=="4"){
+            records = await ChimpTestRecords.find({}).sort({ createdAt: -1 });
+        }
+
+       
         res.status(200).json(records);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -12,27 +31,56 @@ const getAllRecords = async (req, res) => {
 
 // Get top 5 scores of the day
 const getTopScores = async (req, res) => {
+    
+
     try {
+        const screen = req.headers['screen'];
+        // Access the screen parameter
+        
         const startOfDay = new Date();
         startOfDay.setHours(0, 0, 0, 0);
-
-        const topScores = await ReactionRecord.find({})
-            .sort({ score: 1 }) // Sort by score in ascending order (lower score is better)
-            .limit(5);
-
-
+        let topScores;
+        if(screen=="1"){
+            topScores = await DinoJumpRecords.find({}).sort({ score: -1 }).limit(5);
+        }
+        else if(screen=="2"){
+            topScores = await ReactionGameRecords.find({}).sort({ score: 1 }).limit(5);
+        }
+        else if(screen=="3"){
+            topScores = await ColourPuzzleRecords.find({}).sort({ score: -1 }).limit(5);
+        }
+        else if(screen=="4"){
+            topScores = await ChimpTestRecords.find({}).sort({ score: -1 }).limit(5);
+        }
         res.status(200).json(topScores);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
+
 // Save a new score
 const saveScore = async (req, res) => {
     const { name, score } = req.body;
+    const screen = req.headers['screen'];
 
     try {
-        const record = await ReactionRecord.create({ name, score });
+        let record;
+        if(screen=="1"){
+            record = await DinoJumpRecords.create({ name, score });
+        }
+        else if(screen=="2"){
+            record = await ReactionGameRecords.create({ name, score });
+        }
+        else if(screen=="3"){
+            record = await ColourPuzzleRecords.create({ name, score });
+        }
+        else if(screen=="4"){
+            record = await ChimpTestRecords.create({ name, score });
+        }
+
+        
+
         res.status(200).json(record);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -44,22 +92,60 @@ const getUserRank = async (req, res) => {
     const { score } = req.body;
 
     try {
-        const allRecords = await ReactionRecord.find({}).sort({ score: 1 });
+        const screen = req.headers['screen'];
+        console.log(`Screen header value: ${screen}`);
+        let allRecords;
+        
         let rank = 1;
-
-        for (let i = 0; i < allRecords.length; i++) {
-            if (allRecords[i].score <= score) {
-                rank++;
-            } else {
-                break;
+        if (screen=="1"){
+            allRecords = await DinoJumpRecords.find({}).sort({ score: -1 });
+            for (let i = 0; i < allRecords.length; i++) {
+                if (allRecords[i].score >= score) {
+                    rank++;
+                } else {
+                    break;
+                }
             }
         }
+        else if(screen=="2"){
+             allRecords = await ReactionGameRecords.find({}).sort({ score: 1 });
+             for (let i = 0; i < allRecords.length; i++) {
+                if (allRecords[i].score <= score) {
+                    rank++;
+                } else {
+                    break;
+                }
+            }
+        }
+        else if (screen=="3"){
+            allRecords = await ColourPuzzleRecords.find({}).sort({ score: -1 });
+            for (let i = 0; i < allRecords.length; i++) {
+                if (allRecords[i].score >= score) {
+                    rank++;
+                } else {
+                    break;
+                }
+            }}
 
+        else if (screen=="4"){
+            allRecords = await ChimpTestRecords.find({}).sort({ score: -1 });
+            for (let i = 0; i < allRecords.length; i++) {
+                if (allRecords[i].score >= score) {
+                    rank++;
+                } else {
+                    break;
+                }
+            }
+        }
+      
+        
+       
         res.status(200).json({ rank });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
+
 
 module.exports = {
     getAllRecords,
