804e6fabf8456ef6f9d8380ac1dcd0d12626668e
RusiruD <rdha868@aucklanduni.ac.nz>
2024-09-27 17:42:28 +1200

    feat: Ensure scores must be saved with a name

--- frontend/src/components/OverallLeaderboard.js
+++ frontend/src/components/OverallLeaderboard.js
@@ -31,6 +31,7 @@ const OverallLeaderboard = () => {
                 : "N/A"
                                
             ]);
+
             setUserScoresArray(updatedUserScoresArray); 
             
              //sort the userScoresArray by the overall score of each user in descending order
@@ -56,23 +57,23 @@ const OverallLeaderboard = () => {
   
    
     return (
- <div>
-    <div>
-    <table>
-        <thead>
-            <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Overall Score</th>
-                <th>Dino Jump Score</th>
-                <th>Reaction Game Score</th>
-                <th>Colour Puzzle Score</th>
-                <th>Chimp Test Score</th>
-            </tr>
-        </thead>
-        <tbody>
-            {/* Display the user scores, creating a row for each user */}
-        {userScoresArray.length > 0 ? (
+            <div>
+                <div>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Overall Score</th>
+                        <th>Dino Jump Score</th>
+                        <th>Reaction Game Score</th>
+                        <th>Colour Puzzle Score</th>
+                        <th>Chimp Test Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {/* Display the user scores, creating a row for each user */}
+                    {userScoresArray.length > 0 ? (
                         userScoresArray.map((subArray, index) => (
                             <tr key={index}>
                                 <td>{index + 1}</td> {/* Rank */}
